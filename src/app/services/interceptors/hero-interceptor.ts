96cf23d0231c68783bef36b094f0c9e19eaac94a
Facundo Gonzales <facundogonzalesliendo@gmail.com>
2025-02-22 19:03:09 -0300

    se arma interceptor para formateo de data

---
+++ src/app/services/interceptors/hero-interceptor.ts
@@ -0,0 +1,29 @@
+import { HttpEvent, HttpHandlerFn, HttpInterceptorFn, HttpRequest, HttpResponse } from '@angular/common/http';
+import { map, Observable } from 'rxjs';
+import { SuperHero } from '../../models/super-hero-model';
+
+export const heroInterceptorFn: HttpInterceptorFn = (req: HttpRequest<any>, next: HttpHandlerFn): Observable<HttpEvent<any>> => {
+  return next(req).pipe(
+    map((e: HttpEvent<any>) => {
+      if(e instanceof HttpResponse) {
+        if(e && Array.isArray(e.body)) {
+          const formatData = e.body.map(formatHero);
+          return e.clone({ body: formatData })
+        }
+      }
+      return e;
+    })
+  )
+}
+
+export function formatHero(data: any): SuperHero {
+  return {
+    id: data.id,
+    name: data.name,
+    images: data.images,
+    appearance: data.appearance,
+    biography: data.biography,
+    connections: data.connections,
+    powerstats: data.powerstats
+  };
+}
