59a37479360effc24fb7eee8fca266a8fbd88d10
Facundo Gonzales <facundogonzalesliendo@gmail.com>
2025-02-25 17:00:08 -0300

    se agrega translate y estilos

--- src/app/shared/hero-finder/hero-finder.component.ts
+++ src/app/shared/hero-finder/hero-finder.component.ts
@@ -6,11 +6,20 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { CommonModule } from '@angular/common';
 import { TextUppercaseTransformDirective } from '../../directives/text-uppercase-transform.directive';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-hero-finder',
   standalone: true,
-  imports: [ CommonModule, FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule, TextUppercaseTransformDirective ],
+  imports: [ CommonModule,
+             FormsModule,
+             TranslateModule,
+             MatFormFieldModule,
+             MatInputModule,
+             MatButtonModule,
+             MatIconModule,
+             TextUppercaseTransformDirective
+            ],
   templateUrl: './hero-finder.component.html',
   styleUrl: './hero-finder.component.scss'
 })
@@ -18,6 +27,11 @@ export class HeroFinderComponent {
   @Output() emitName = new EventEmitter();
 
   public valueFilter: string = '';
+
+  constructor(public translate: TranslateService) {
+    this.translate.setDefaultLang('es');
+    this.translate.use('es');
+  }
 
   public filtered(): void {
     this.emitName.emit(this.valueFilter);
