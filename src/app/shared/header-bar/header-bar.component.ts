2a59a1bb64ac21727a68f2cad87686e089115563
Facundo Gonzales <facundogonzalesliendo@gmail.com>
2025-02-25 10:24:02 -0300

    se modifica header bar para ser reutilizable

--- src/app/shared/header-bar/header-bar.component.ts
+++ src/app/shared/header-bar/header-bar.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header-bar',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './header-bar.component.html',
   styleUrl: './header-bar.component.scss'
 })
 export class HeaderBarComponent {
+  @Input() title: string = '';
 
+  constructor(public translate: TranslateService) {
+    this.translate.setDefaultLang('es');
+    this.translate.use('es');
+  }
 }
