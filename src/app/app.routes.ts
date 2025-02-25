f943d2e99231e90d31e6510c365a8dfcc61afae3
Facundo Gonzales <facundogonzalesliendo@gmail.com>
2025-02-24 21:24:45 -0300

    se arman rutas de navegacion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '', redirectTo: 'home', pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./pages/super-heroes-page/super-heroes-page.component').then(p => p.SuperHeroesPageComponent)
+  },
+  {
+    path: 'create',
+    loadComponent: () => import('./pages/edit-super-hero/edit-super-hero.component').then(p => p.EditSuperHeroComponent)
+  },
+  {
+    path: 'edit',
+    loadComponent: () => import('./pages/edit-super-hero/edit-super-hero.component').then(p => p.EditSuperHeroComponent)
+  },
+  {
+    path: '**',
+    loadComponent: () => import('./pages/super-heroes-page/super-heroes-page.component').then(p => p.SuperHeroesPageComponent)
+  }
+];
