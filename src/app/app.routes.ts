1406fbb58820e6cb761f97f961464ebc383562e3
Facundo Gonzales <facundogonzalesliendo@gmail.com>
2025-02-25 12:42:42 -0300

    se agregan parametros en rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,7 +13,7 @@ export const routes: Routes = [
     loadComponent: () => import('./pages/edit-super-hero/edit-super-hero.component').then(p => p.EditSuperHeroComponent)
   },
   {
-    path: 'edit',
+    path: 'edit/:id',
     loadComponent: () => import('./pages/edit-super-hero/edit-super-hero.component').then(p => p.EditSuperHeroComponent)
   },
   {
