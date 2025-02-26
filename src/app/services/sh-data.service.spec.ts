17884dea50e59fbe38fd5500b7ed51340bb996d8
Facundo Gonzales <facundogonzalesliendo@gmail.com>
2025-02-26 10:09:18 -0300

    se hacen importaciones de modulos

--- src/app/services/sh-data.service.spec.ts
+++ src/app/services/sh-data.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ShDataService } from './sh-data.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('ShDataService', () => {
   let service: ShDataService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(ShDataService);
   });
 
