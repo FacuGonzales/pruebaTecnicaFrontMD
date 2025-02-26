260bd1f87e68ed17cbffa7e744acd192117d9d66
Facundo Gonzales <facundogonzalesliendo@gmail.com>
2025-02-26 12:40:06 -0300

    se agregan mas mocks para test

--- src/assets/mocks/mocks.ts
+++ src/assets/mocks/mocks.ts
@@ -34,34 +34,34 @@ export const MOCK_SUPER_HEROES: SuperHero[] = [
     }
   },
   {
-    id: 71,
-    name: "Batman II",
+    id: 200,
+    name: "Daphne Powell",
     powerstats: {
-      intelligence: 88,
-      strength: 11,
-      speed: 33,
-      durability: 28,
-      power: 36,
-      combat: 100
+    intelligence: 100,
+    strength: 100,
+    speed: 100,
+    durability: 100,
+    power: 100,
+    combat: 84
     },
     appearance: {
       gender: "Male",
-      race: "Human",
-      height: "5'10",
-      weight: "175 lb",
+      race: "God / Eternal",
+      height: "-",
+      weight: "- lb",
     },
     biography: {
-      fullName: "Dick Grayson",
-      placeOfBirth: "-",
-      firstAppearance: "-",
-      publisher: "Nightwing",
+      fullName: "",
+      placeOfBirth: "Created in the Beyond",
+      firstAppearance: "Marvel Super-Heroes Secret Wars #1",
+      publisher: "Marvel Comics",
       alignment: "good"
     },
     images: {
-      xs: "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/xs/71-batman-ii.jpg",
-      sm: "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/sm/71-batman-ii.jpg",
-      md: "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/md/71-batman-ii.jpg",
-      lg: "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/lg/71-batman-ii.jpg"
+      xs: "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/xs/200-daphne-powell.jpg",
+      sm: "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/sm/200-daphne-powell.jpg",
+      md: "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/md/200-daphne-powell.jpg",
+      lg: "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/lg/200-daphne-powell.jpg"
     }
   }
 ]
@@ -69,6 +69,72 @@ export const MOCK_SUPER_HEROES: SuperHero[] = [
 export const MOCK_CREATE_HERO = {
   id: 1,
   name: "Chapulin",
+  powerstats: {
+    intelligence: 88,
+    strength: 11,
+    speed: 33,
+    durability: 28,
+    power: 36,
+    combat: 100
+  },
+  appearance: {
+    gender: "Male",
+    race: "Human",
+    height: "5'10",
+    weight: "175 lb",
+  },
+  biography: {
+    fullName: "Chespirito",
+    placeOfBirth: "-",
+    firstAppearance: "-",
+    publisher: "Nightwing",
+    alignment: "good"
+  },
+  images: {
+    xs: "",
+    sm: "",
+    md: "",
+    lg: ""
+  }
+}
+
+
+export const MOCK_UPDATE_HERO = {
+  id: 70,
+  name: "BATMAAAAN",
+  powerstats: {
+    intelligence: 88,
+    strength: 11,
+    speed: 33,
+    durability: 28,
+    power: 36,
+    combat: 100
+  },
+  appearance: {
+    gender: "Male",
+    race: "Human",
+    height: "5'10",
+    weight: "175 lb",
+  },
+  biography: {
+    fullName: "Chespirito",
+    placeOfBirth: "-",
+    firstAppearance: "-",
+    publisher: "Nightwing",
+    alignment: "good"
+  },
+  images: {
+    xs: "",
+    sm: "",
+    md: "",
+    lg: ""
+  }
+}
+
+
+export const MOCK_NO_EXISTE_HERO = {
+  id: 2,
+  name: "POPEYE",
   powerstats: {
     intelligence: 88,
     strength: 11,
